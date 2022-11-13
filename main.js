23f663b21d45faff5623194217a55bcfadf66bdb
Kane <LisaxKane@gmail.com>
2022-11-13 01:20:02 -0600

    added min max function and delete node function

--- main.js
+++ main.js
@@ -12,14 +12,14 @@ class Node {
 
 class Tree {
     constructor(array) {
-        array = this.array;
-        this.root = null;
+        this.array = array.sort((a, b) => a - b);
+        this.root = this.buildTree(this.array,0,this.array.length -1)
         
     }
    
     buildTree(array, start, end) {
         
-        array = array.sort((a, b) => a - b);
+       
         
         
         if (start > end) {
@@ -32,51 +32,96 @@ class Tree {
         return root;
     }
 
-    insert(key) {
-        let newNode = new Node(key);
-        if (!this.root) {
-            this.root = newNode;
-            return this;
-        }
-        let tree = this.root;
-
-        while (true) {
-            if (key < tree.data) {
-                if (!tree.left) {
-                    tree.left = newNode;
-                    return this;
-                }
-                tree = tree.left;
-            }
-            else {
-                if (!tree.right) {
-                    tree.right = newNode;
-                    return this;
-                }
-                tree = tree.right;
-                
-            }
-            
-        }
-     
-        return this;
+    insert(data) {
+        const newNode = new Node(data);
+        this.insertNode(this.root, newNode)
+        this.array.push(data)
+      
     
     }
+
+    insertNode(root, newNode) {
+        if (newNode.data < root.data) {
+            if (root.left === null) {
+                root.left = newNode
+            }
+            else {
+                this.insertNode(root.left, newNode)
+            }
+        }
+        else {
+            if (root.right === null) {
+                root.right = newNode
+            }
+            else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+    delete(data) {
+        let index = this.array.indexOf(data)
+        if (index > -1) {
+            this.array.splice(index,1)
+        }
+        this.root = this.deleteRec(this.root, data)
+        return this;
+       
+    }
+    deleteRec(root, data) {
+        if (root == null) {
+            return root;
+        }
+        if (data < root.data) {
+            root.left = this.deleteRec(root.left, data)
+        }
+        else if (data > root.data) {
+            root.right = this.deleteRec(root.right, data)
+        }
+        else {
+            if (!root.left && !root.right) {
+                return null
+            }
+
+            if (!root.left) {
+             return root.right
+            }
+            else if (!root.right) {
+                return root.left;
+            }
+            root.data = this.min(root.right)
+            root.right = this.deleteRec(root.right, root.data)
+        }  
+        return root;
+    }
+    min(root) {
+        if (!root.left) {
+            return root.data
+        }
+        else {
+            return this.min(root.left)
+        }
+    }
+    max(root) {
+        if (!root.right) {
+            return root.data;
+        }
+        else {
+            return this.max(root.right)
+        }
+    }
+    find(data) {
+        if (this.root == null) {
+            return this.root;
+        }
+        if (data < this.root.data) {
+            this.root = this.root.left;
+        }
+        else if (data > this.root.data) {
+            this.root = root.right;
+        }
+        return this.root;
+    }
 }
-
-testArr = [1,2,19,4,5,6,7,8,9,1000,2000,3000,4000,5000]
-testArr.push(200)
-testArr.push(201)
-testArr.push(800)
-testArr.push(123)
-testArr.push(123)
-testArr.push(222)
-
-const tree1 = new Tree(testArr)
-
-
-tree1.insert(2000)
-
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.right !== null) {
@@ -87,6 +132,29 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
 }
-console.log(tree1.buildTree(testArr, 0 , testArr.length ))
-  
-prettyPrint(tree1.buildTree(testArr,0,testArr.length - 1))
+
+let testArr = [345,1,2,3,4,5,6,7,8,9]
+
+
+let tree1 = new Tree(testArr)
+
+
+tree1.insert(313)
+tree1.buildTree(testArr,0,testArr.length - 1)
+
+
+tree1.insert(12312)
+
+console.log(testArr)
+
+tree1.delete(345)
+console.log(tree1.min(tree1.buildTree(testArr, 0, testArr.length)))
+
+
+
+
+
+prettyPrint(tree1.buildTree(testArr, 0 , testArr.length -1))
+
+
+
